dc7819465fc065260fe7d6d2faf1c72d48668377
Jáchym Toušek <enumag@gmail.com>
2014-09-07 01:53:05 +0200

    Fixed parameters resolution of generated factories (added fix)

--- src/DI/ContainerBuilder.php
+++ src/DI/ContainerBuilder.php
@@ -306,7 +306,7 @@ class ContainerBuilder extends Nette\Object
 
 		if (!$def->parameters) {
 			$ctorParams = array();
-			if (!$def->factory && $def->class) {
+			if (!$def->factory || !$def->factory->entity && !$def->factory->arguments) {
 				$def->setFactory($def->class);
 			}
 			if ($def->factory && !$def->factory->arguments && ($class = $this->resolveEntityClass($def->factory, array($name => 1)))
